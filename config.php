99789523db5bf4b82de577c7bee3c7cc91ff5711
Satura Izhato Rufi <140029924+AyamGorengMadura@users.noreply.github.com>
2023-09-22 23:01:06 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+$user_name = "root";
+$password = "";
+$database = "db_project";
+$host_name = "localhost";
+mysql_connect($host_name, $user_name, $password);
+echo "Connection opened";
+
+?>
---
+++ register.php
@@ -0,0 +1,72 @@
+<?script
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Buat Akun</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+
+  <!-- card form -->
+
+  <div class="d-flex justify-content-center">
+    <div class="col-8 ms-auto me-auto text-center border-3 bg-primary rounded-3">
+
+        <h1>Buat Akun</h1>
+        <h5><i>Create Account</i></h5>
+
+        <form action="" class="">
+
+  <div class="col-9 ms-auto me-auto text-center">
+    
+    <div class="input-group flex-nowrap pb-3 pt-5">
+      <span class="input-group-text" id="addon-wrapping">!</span>
+      <input type="text" class="form-control" placeholder="Name">
+    </div>
+    
+    <div class="input-group flex-nowrap pb-3">
+      <span class="input-group-text" id="addon-wrapping">!</span>
+      <input type="text" class="form-control" placeholder="Email">
+    </div>
+
+    <div class="input-group flex-nowrap pb-3">
+      <span class="input-group-text" id="addon-wrapping">!</span>
+      <input type="text" class="form-control" placeholder="Password">
+    </div>
+
+    <div class="input-group flex-nowrap pb-3">
+      <span class="input-group-text" id="addon-wrapping">!</span>
+      <input type="text" class="form-control" placeholder="Username">
+    </div>
+
+    <div class="input-group flex-nowrap pb-3">
+      <span class="input-group-text" id="addon-wrapping">!</span>
+      <input type="text" class="form-control" placeholder="Number">
+    </div>
+
+    <!-- gender radio -->
+
+    <div>
+
+    </div>
+
+    <div class="pb-5">
+    <button type="submit" class="btn btn-light">Submit</button>
+    </div>
+
+  </div>
+        </form>
+
+    </div>
+  </div>
+
+</body>
+</html>
