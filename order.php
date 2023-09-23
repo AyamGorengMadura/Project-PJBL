56ea07c4e559417794f7757a74d1e64a3f5795de
Satura Izhato Rufi <140029924+AyamGorengMadura@users.noreply.github.com>
2023-09-23 11:28:48 +0700

    Add files via upload

---
+++ order.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Order</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <!-- card order form -->
+
+    <div class="col-9 mx-auto bg-primary "> <!-- wrapper --> 
+    <div> <!-- container -->
+
+        <div class="text-center pt-5">
+            <h1 class="fs-1">Order</h1>
+            <p class=></p>
+        </div>
+
+        <form action="" class="">
+
+        <div class="input-group flex-nowrap ">
+            <span class="input-group-text" id="addon-wrapping">@</span>
+            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
+
+
+        </form>
+    </div>
+    </div>
+
+</body>
+</html>
